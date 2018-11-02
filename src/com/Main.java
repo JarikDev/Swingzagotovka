13315bf948df5766696db098f746b3e9cbd040b3
ivan4000 <sxln1@mail.ru>
2018-11-02 20:28:42 +0300

    SwingTemplateZagotovka

---
+++ src/com/Main.java
@@ -0,0 +1,25 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
+
+    public static void main(String[] args) {
+        jFrame.add(jPanel);
+
+    }
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {
+        };
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 500, 500);
+        return jFrame;
+
+    }
+}
